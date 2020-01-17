01286798b549f28276b28a18301ba026c4ea236c
Benoit <ben63112@gmail.com>
2020-01-17 15:39:34 +0100

    Entity framework + BddContext

---
+++ PortailReserve/PortailReserve/DAL/BddContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PortailReserve.DAL
+{
+    public class BddContext : DbContext
+    {
+        public BddContext ()
+        {
+
+        }
+
+        public BddContext Create ()
+        {
+            return new BddContext();
+        }
+    }
+}
