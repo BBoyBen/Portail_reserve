8b29795a3119e821711b5106ef7e208a4b7aff15
Benoit <ben63112@gmail.com>
2020-01-19 16:11:53 +0100

    Mise en place pc 2

---
+++ PortailReserve/PortailReserve/Models/BddContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortailReserve.Models
+{
+    public class BddContext : DbContext
+    {
+    }
+}
