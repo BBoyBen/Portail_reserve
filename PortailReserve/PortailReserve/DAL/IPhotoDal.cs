5697c3a37402e198112004babf517b04b121b96f
Benoit <ben63112@gmail.com>
2020-01-24 12:39:04 +0100

    Dal photo

--- PortailReserve/PortailReserve/DAL/IPhotoDal.cs
+++ PortailReserve/PortailReserve/DAL/IPhotoDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PortailReserve.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace PortailReserve.DAL
 {
     public interface IPhotoDal : IDisposable
     {
+        Guid AjouterPhoto(Photo photo);
+        Photo GetPhotoById(Guid id);
+        List<Photo> GetAllPhotos();
+        List<Photo> GetPhotosByAlbum(Guid idAlbum);
+        int SupprimerPhoto(Guid id);
     }
 }
--- PortailReserve/PortailReserve/DAL/Impl/PhotoDal.cs
+++ PortailReserve/PortailReserve/DAL/Impl/PhotoDal.cs
@@ -15,9 +15,83 @@ namespace PortailReserve.DAL.Impl
             bdd = new BddContext();
         }
 
+        public Guid AjouterPhoto(Photo photo)
+        {
+            try
+            {
+                photo.Ajout = DateTime.Now;
+
+                bdd.Photos.Add(photo);
+                bdd.SaveChanges();
+
+                return bdd.Photos.ToList().Last().Id;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur ajout de photo -> " + e);
+            }
+        }
+
         public void Dispose()
         {
             bdd.Dispose();
+        }
+
+        public List<Photo> GetAllPhotos()
+        {
+            try
+            {
+                List<Photo> all = bdd.Photos.ToList();
+                return all;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur récupération de toutes les photos -> " + e);
+                return new List<Photo>();
+            }
+        }
+
+        public Photo GetPhotoById(Guid id)
+        {
+            try
+            {
+                Photo photo = bdd.Photos.FirstOrDefault(p => p.Id.Equals(id));
+                return photo;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur récupération photo par id : " + id + " -> " + e);
+                return null;
+            }
+        }
+
+        public List<Photo> GetPhotosByAlbum(Guid idAlbum)
+        {
+            try
+            {
+                List<Photo> byAlbum = bdd.Photos.Where(p => p.Album.Id.Equals(idAlbum)).ToList();
+                return byAlbum;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur récupération des photos de l'album id : " + idAlbum + " -> " + e);
+                return new List<Photo>();
+            }
+        }
+
+        public int SupprimerPhoto(Guid id)
+        {
+            try
+            {
+                Photo toDelete = GetPhotoById(id);
+                if (toDelete == null)
+                    return 0;
+
+                bdd.Photos.Remove(toDelete);
+                bdd.SaveChanges();
+
+                return 1;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur suppression photo id : " + id + " -> " + e);
+                return -1;
+            }
         }
     }
 }
