63b5785e6d3358c4a921e14a71af0440fefdd71a
Benoit <ben63112@gmail.com>
2020-01-24 09:06:50 +0100

    Dal cours

--- PortailReserve/PortailReserve/DAL/ICoursDal.cs
+++ PortailReserve/PortailReserve/DAL/ICoursDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PortailReserve.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace PortailReserve.DAL
 {
     public interface ICoursDal : IDisposable
     {
+        Guid AjouterCours(Cours cours);
+        Cours GetCoursById(Guid id);
+        int ModifierCours(Guid id, Cours cours);
+        int SupprimerCours(Guid id);
+        List<Cours> GetAllCours();
+        List<Cours> GetCoursByTheme(string theme);
     }
 }
--- PortailReserve/PortailReserve/DAL/Impl/CoursDal.cs
+++ PortailReserve/PortailReserve/DAL/Impl/CoursDal.cs
@@ -15,9 +15,109 @@ namespace PortailReserve.DAL.Impl
             bdd = new BddContext();
         }
 
+        public Guid AjouterCours(Cours cours)
+        {
+            try
+            {
+                cours.Publication = DateTime.Now;
+                cours.Modification = DateTime.Now;
+
+                bdd.Cours.Add(cours);
+                bdd.SaveChanges();
+
+                bdd.Cours.ToList().Last();
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur ajouter un cours -> " + e);
+                return Guid.Empty;
+            }
+        }
+
         public void Dispose()
         {
             bdd.Dispose();
+        }
+
+        public List<Cours> GetAllCours()
+        {
+            try
+            {
+                List<Cours> all = bdd.Cours.ToList();
+                return all;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur rcupération de tous les cours -> " + e);
+                return new List<Cours>();
+            }
+        }
+
+        public Cours GetCoursById(Guid id)
+        {
+            try
+            {
+                Cours cours = bdd.Cours.FirstOrDefault(c => c.Id.Equals(id));
+                return cours;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur récupération cours id : " + id + " -> " + e);
+                return null;
+            }
+        }
+
+        public List<Cours> GetCoursByTheme(string theme)
+        {
+            try
+            {
+                List<Cours> coursByTheme = bdd.Cours.Where(a => a.Theme.Equals(theme)).ToList();
+                return coursByTheme;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur récupération des cours par theme : " + theme + " -> " + e);
+                return new List<Cours>();
+            }
+        }
+
+        public int ModifierCours(Guid id, Cours cours)
+        {
+            try
+            {
+                Cours toModify = GetCoursById(id);
+                if (toModify == null)
+                    return 0;
+
+                toModify.Description = cours.Description;
+                toModify.Fichier = cours.Fichier;
+                toModify.Nom = cours.Nom;
+                toModify.Theme = cours.Theme;
+                toModify.Modification = DateTime.Now;
+
+                bdd.SaveChanges();
+
+                return 1;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur modification cours id : " + id + " -> " + e);
+                return -1;
+            }
+        }
+
+        public int SupprimerCours(Guid id)
+        {
+            try
+            {
+                Cours toDelete = GetCoursById(id);
+                if (toDelete == null)
+                    return 0;
+
+                bdd.Cours.Remove(toDelete);
+                bdd.SaveChanges();
+
+                return 1;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Erreur suppression cours id : " + id + " -> " + e);
+                return -1;
+            }
         }
     }
 }
