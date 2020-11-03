358633d91567da029b9208ad588e436308075f54
Benoit <ben63112@gmail.com>
2020-11-03 17:58:45 +0100

    Instanciation controller des cours

---
+++ PortailReserve/PortailReserve/Controllers/CoursController.cs
@@ -0,0 +1,50 @@
+﻿using PortailReserve.DAL;
+using PortailReserve.DAL.Impl;
+using PortailReserve.Models;
+using PortailReserve.Models.NullObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace PortailReserve.Controllers
+{
+    public class CoursController : Controller
+    {
+        private IUtilisateurDal uDal;
+        private ICoursDal cDal;
+
+        public CoursController()
+        {
+            uDal = new UtilisateurDal();
+            cDal = new CoursDal();
+        }
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            Utilisateur u = uDal.GetUtilisateurById(HttpContext.User.Identity.Name);
+            if (u == null)
+            {
+                FormsAuthentication.SignOut();
+                return RedirectToAction("Index", "Login");
+            }
+            if (u.Equals(typeof(UtilisateurNull)))
+            {
+                FormsAuthentication.SignOut();
+                ViewBag.Erreur = ((UtilisateurNull)u).Error;
+                return RedirectToAction("Index", "Login");
+            }
+            if (u.PremiereCo)
+                return RedirectToAction("PremiereCo", "Login");
+
+            ViewBag.Grade = u.Grade;
+            ViewBag.Nom = u.Nom.ToUpperInvariant();
+            ViewBag.Role = u.Role;
+
+            return View();
+        }
+    }
+}
